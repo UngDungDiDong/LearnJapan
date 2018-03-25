b59d0aec28646f740026364ffd58259f2a9566fd
thudan <lethithudan@gmail.com>
2018-03-25 09:40:44 +0700

    dan commit

--- app/src/main/java/com/japan/jav/learnjapan/create_account_dan/CreateAccountActivity.java
+++ app/src/main/java/com/japan/jav/learnjapan/create_account_dan/CreateAccountActivity.java
@@ -1,17 +1,76 @@
 package com.japan.jav.learnjapan.create_account_dan;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.japan.jav.learnjapan.R;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.UUID;
+import java.util.regex.Pattern;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by matas on 3/19/18.
  */
 
 public class CreateAccountActivity extends AppCompatActivity {
+    EditText edtUsername, edtPass, edtEmail, edtConfirmPass;
+    TextView txtCheckUser, txtCheckPass, txtCheckEmail, txtConfirmPass;
+    Button btnSignUp;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        edtUsername= (EditText) findViewById(R.id.edt_username);
+        edtEmail = (EditText) findViewById(R.id.edt_email);
+        edtPass =(EditText) findViewById(R.id.edt_password);
+        edtConfirmPass =(EditText) findViewById(R.id.edt_confirm_password);
+        btnSignUp = (Button) findViewById(R.id.btnSignup);
+        txtCheckUser = (TextView) findViewById(R.id.txtUsername);
+        txtCheckEmail = (TextView) findViewById(R.id.txtEmail);
+        txtCheckPass = (TextView) findViewById(R.id.txtPassword);
+        txtConfirmPass = (TextView) findViewById(R.id.txtConfirmPass);
+
     }
 }

bc16d6f3e34b0269aaf814b0161524142e9de970
havoanhnguyen <havoanhnguyen@github.com>
2018-03-25 19:25:44 +0700

    loginFB

--- app/src/main/java/com/japan/jav/learnjapan/login_diem_nguyen/LoginActivity.java
+++ app/src/main/java/com/japan/jav/learnjapan/login_diem_nguyen/LoginActivity.java
@@ -1,7 +1,13 @@
 package com.japan.jav.learnjapan.login_diem_nguyen;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,7 +15,20 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.japan.jav.learnjapan.R;
+import org.json.JSONObject;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 /**
  * Created by matas on 3/17/18.
@@ -20,25 +39,69 @@ public class LoginActivity  extends AppCompatActivity {
     private EditText edtUsername;
     private EditText edtPassword;
     private Button btnLogin;
-    private ImageView imgFacebook;
+    private LoginButton imgFacebook;
     private ImageView imgGoogle;
     private TextView txtCreateAcount;
     private TextView txtForgotPass;
 
+    CallbackManager callbackManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
         setContentView(R.layout.activity_login_nguyen_diem);
 
         getControls();
         setEvens();
+        keyHashFB();
+
+        imgFacebook.setReadPermissions(Arrays.asList("public_profile","email"));
+        setLoginFB();
+    }
+
+    private void setLoginFB() {
+        imgFacebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // login sucessfull
+                imgFacebook.setVisibility(View.INVISIBLE);
+                result();
+            }
+
+            @Override
+            public void onCancel() {
+                // return if login sucessfull
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                // login fail
+            }
+        });
+    }
+
+    private void result() {
+        GraphRequest graphRequest = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                Log.d("aaa",response.getJSONObject().toString());
+            }
+        });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "name,email");
+        graphRequest.setParameters(parameters);
+        graphRequest.executeAsync();
+
     }
 
     private void getControls() {
         edtUsername = (EditText) findViewById(R.id.edt_username);
         edtPassword = (EditText) findViewById(R.id.edt_password);
         btnLogin = (Button) findViewById(R.id.btnLogin);
-        imgFacebook = (ImageView) findViewById(R.id.img_facebook);
+        imgFacebook = (LoginButton) findViewById(R.id.img_facebook);
         imgGoogle = (ImageView) findViewById(R.id.img_google);
         txtCreateAcount = (TextView) findViewById(R.id.tv_create_an_account);
         txtForgotPass = (TextView) findViewById(R.id.tv_forgot_password);
@@ -80,5 +143,30 @@ public class LoginActivity  extends AppCompatActivity {
                 // go forgot Password
             }
         });
+    }
+
+    private void keyHashFB(){
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    getPackageName(),
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        }
+        catch (PackageManager.NameNotFoundException e) {
+
+        }
+        catch (NoSuchAlgorithmException e) {
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }

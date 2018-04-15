07639eba5d586cb23fc5dc5d54a8c0d903622fdc
ducxomme <ducxomme@gmail.com>
2018-04-15 10:56:55 +0700

    nothing

--- app/src/main/java/com/japan/jav/learnjapan/home_navigation_nhi_tam/fragment/MojiFragment.java
+++ app/src/main/java/com/japan/jav/learnjapan/home_navigation_nhi_tam/fragment/MojiFragment.java
@@ -5,7 +5,6 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.databinding.DataBindingUtil;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;

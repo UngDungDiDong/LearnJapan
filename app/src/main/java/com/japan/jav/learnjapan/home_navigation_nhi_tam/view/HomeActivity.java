67c8afa320dba6c630a39e3cbd191233214ca089
ducxomme <ducxomme@gmail.com>
2018-04-23 10:28:22 +0700

    add change nothing

--- app/src/main/java/com/japan/jav/learnjapan/home_navigation_nhi_tam/view/HomeActivity.java
+++ app/src/main/java/com/japan/jav/learnjapan/home_navigation_nhi_tam/view/HomeActivity.java
@@ -180,9 +180,9 @@ public class HomeActivity extends AppCompatActivity implements NetworkListener{
                         Intent intent = new Intent(HomeActivity.this, ProfileActivity.class);
                         startActivity(intent);
                         return true;
-                    case R.id.about_us:
+//                    case R.id.about_us:
 //                        Toast.makeText(getApplicationContext(), "About_us ai lam", Toast.LENGTH_SHORT).show();
-                        return true;
+//                        return true;
                     case R.id.sent_mail:
                         Toast.makeText(getApplicationContext(), "Logout", Toast.LENGTH_SHORT).show();
                         mAuth.signOut();
